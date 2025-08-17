71be23ffcf032a63f2ba652c196c2f7c7c61e3e9
AbdullahWahdan <115044888+AbdullahWahdan@users.noreply.github.com>
2025-08-17 21:46:09 +0300

    f

--- Terraform/team-01/6-main.tf
+++ Terraform/team-01/6-main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "stagebucket12"
-    key            = "/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true

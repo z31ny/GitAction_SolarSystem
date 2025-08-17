b732982fdbee3e98dbd336e6e718839e0300b801
AbdullahWahdan <115044888+AbdullahWahdan@users.noreply.github.com>
2025-08-17 20:51:25 +0300

    hh

---
+++ Terraform/team-01/3-s3main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "terraform_states" {
+  bucket = "terraform-states-wahdan"
+
+  tags = {
+    Name        = "TerraformStateBucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_states.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "TerraformLockTable"
+  }
+}

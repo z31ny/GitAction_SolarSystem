405fff6cc790d5767284dbffb468cb525639142f
AbdullahWahdan <115044888+AbdullahWahdan@users.noreply.github.com>
2025-08-17 21:09:53 +0300

    final one inshallah

--- Terraform/team-01/5-backend.tf
+++ Terraform/team-01/5-backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "terraform-states-wahdan-03"
-#    key            = "terraform.tfstate"
-#    region        = "us-east-1"
-#    dynamodb_table = "terraform-locks-2"
-#    encrypt       = true
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket         = "terraform-states-wahdan-03"
+    key            = "terraform.tfstate"
+    region        = "us-east-1"
+    dynamodb_table = "terraform-locks-2"
+    encrypt       = true
+  }
+}

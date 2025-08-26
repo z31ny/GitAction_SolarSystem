67385a27872c5e7179207f9b5ab9956c201c2c4a
AbdullahWahdan <amwahdan24@gmail.com>
2025-08-26 21:44:52 +0300

    s3

--- Terraform/team-01/6-main.tf
+++ Terraform/team-01/6-main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "wahdan-state-bucket-2"
+    bucket         = "amwahdan-state-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-1"
+    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }

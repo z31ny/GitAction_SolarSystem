2183ccc1bff9c4a3945f265ca21262c6976a1e93
AbdullahWahdan <amwahdan24@gmail.com>
2025-08-26 22:03:43 +0300

    fixing tf

--- Terraform/team-01/6-main.tf
+++ Terraform/team-01/6-main.tf
@@ -20,7 +20,7 @@ module "vpc" {
 module "eks" {
   source          = "../infrastructure-modules/eks"
 
-  cluster_name    = "sprints-cluster-0"
+  cluster_name    = "GP-cluster"
   cluster_version = "1.33"
   vpc_id         = module.vpc.vpc_id
   subnet_ids     = module.vpc.private_subnet_ids
